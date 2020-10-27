46edbce910d26ffba56e450c1b2d274c43f98344
AsesorLGVR <68026156+AsesorLGVR@users.noreply.github.com>
2020-10-27 02:50:14 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+let hint = ""
+let answer = ""
+let solution = ""
+info.setLife(3)
+solution = String.fromCharCode(randint(97, 122))
+while (info.life() > 0) {
+    answer = game.askForString("Estoy pensando en la letra...", 1)
+    if (answer == solution) {
+        game.splash("Ganaste")
+        control.reset()
+    } else {
+        info.changeLifeBy(-1)
+        if (info.life() > 0) {
+            hint = "Más arriba"
+            if (answer > solution) {
+                hint = "Más abajo"
+            }
+            game.splash("Prueba " + hint)
+        } else {
+            game.splash("Lo siento, era la letra\"" + solution + "\"")
+        }
+    }
+}
+game.over()
